204af32e4e82f44a1603146456a3c1c4d8c92df8
Tran Van Phuc <puyedo@gmail.com>
2016-11-12 10:55:31 +0700

    Add feature: Resize form

--- FontMananger/UI/frmMain.Designer.cs
+++ FontMananger/UI/frmMain.Designer.cs
@@ -30,15 +30,28 @@
         {
             this.pnlTitle = new System.Windows.Forms.Panel();
             this.tblPnlBody = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlListFont = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlShowContent = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnlBorder = new System.Windows.Forms.Panel();
+            this.tblPnlBody.SuspendLayout();
+            this.pnlMenu.SuspendLayout();
+            this.pnlListFont.SuspendLayout();
+            this.pnlShowContent.SuspendLayout();
+            this.pnlBorder.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlTitle
             // 
             this.pnlTitle.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitle.Location = new System.Drawing.Point(5, 5);
             this.pnlTitle.Name = "pnlTitle";
-            this.pnlTitle.Size = new System.Drawing.Size(1182, 40);
+            this.pnlTitle.Padding = new System.Windows.Forms.Padding(3);
+            this.pnlTitle.Size = new System.Drawing.Size(1180, 40);
             this.pnlTitle.TabIndex = 0;
             // 
             // tblPnlBody
@@ -47,24 +60,107 @@
             this.tblPnlBody.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tblPnlBody.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tblPnlBody.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tblPnlBody.Controls.Add(this.pnlMenu, 0, 0);
+            this.tblPnlBody.Controls.Add(this.pnlListFont, 1, 0);
+            this.tblPnlBody.Controls.Add(this.pnlShowContent, 2, 0);
             this.tblPnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tblPnlBody.Location = new System.Drawing.Point(0, 0);
+            this.tblPnlBody.Location = new System.Drawing.Point(5, 45);
             this.tblPnlBody.Name = "tblPnlBody";
             this.tblPnlBody.RowCount = 1;
             this.tblPnlBody.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblPnlBody.Size = new System.Drawing.Size(1182, 653);
+            this.tblPnlBody.Size = new System.Drawing.Size(1180, 640);
             this.tblPnlBody.TabIndex = 0;
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.Controls.Add(this.label1);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(236, 640);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pnlListFont
+            // 
+            this.pnlListFont.Controls.Add(this.label2);
+            this.pnlListFont.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlListFont.Location = new System.Drawing.Point(236, 0);
+            this.pnlListFont.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlListFont.Name = "pnlListFont";
+            this.pnlListFont.Size = new System.Drawing.Size(236, 640);
+            this.pnlListFont.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // pnlShowContent
+            // 
+            this.pnlShowContent.Controls.Add(this.label3);
+            this.pnlShowContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlShowContent.Location = new System.Drawing.Point(472, 0);
+            this.pnlShowContent.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlShowContent.Name = "pnlShowContent";
+            this.pnlShowContent.Size = new System.Drawing.Size(708, 640);
+            this.pnlShowContent.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
+            // 
+            // pnlBorder
+            // 
+            this.pnlBorder.AllowDrop = true;
+            this.pnlBorder.Controls.Add(this.tblPnlBody);
+            this.pnlBorder.Controls.Add(this.pnlTitle);
+            this.pnlBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBorder.Location = new System.Drawing.Point(5, 5);
+            this.pnlBorder.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlBorder.Name = "pnlBorder";
+            this.pnlBorder.Padding = new System.Windows.Forms.Padding(5);
+            this.pnlBorder.Size = new System.Drawing.Size(1190, 690);
+            this.pnlBorder.TabIndex = 1;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 653);
-            this.Controls.Add(this.pnlTitle);
-            this.Controls.Add(this.tblPnlBody);
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Controls.Add(this.pnlBorder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MinimumSize = new System.Drawing.Size(1200, 700);
             this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Font Manager";
+            this.tblPnlBody.ResumeLayout(false);
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlMenu.PerformLayout();
+            this.pnlListFont.ResumeLayout(false);
+            this.pnlListFont.PerformLayout();
+            this.pnlShowContent.ResumeLayout(false);
+            this.pnlShowContent.PerformLayout();
+            this.pnlBorder.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,5 +169,12 @@
 
         private System.Windows.Forms.Panel pnlTitle;
         private System.Windows.Forms.TableLayoutPanel tblPnlBody;
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlListFont;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pnlShowContent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel pnlBorder;
     }
 }
--- FontMananger/UI/frmMain.cs
+++ FontMananger/UI/frmMain.cs
@@ -4,35 +4,132 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace FontMananger.UI
 {
     public partial class frmMain : Form
     {
+        // Khai bao cac thanh phan su dung cho viec Keo' di chuyen form
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
         public frmMain()
         {
             InitializeComponent();
-            
-            tblPnlBody.CellPaint += TableLayoutPanel1_CellPaint;
+
+            pnlMenu.BackColor = Color.FromArgb(149, 165, 166);
+            pnlListFont.BackColor = Color.FromArgb(189, 195, 199);
+            pnlShowContent.BackColor = Color.FromArgb(236, 240, 241);
+
+            // Them callback cho viec Keo' di chuyen form
+            pnlTitle.MouseDown += PnlTitle_MouseDown;
+
+            SetFormBorderColor(Color.DarkCyan);
+
+            MakeFormBackgroundTransparent(Color.MediumAquamarine);
         }
 
-        private void TableLayoutPanel1_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        private void MakeFormBackgroundTransparent(Color colorNeedToAvoid)
         {
-            if (e.Column == 0 && e.Row == 0)
+            this.BackColor = colorNeedToAvoid;
+            this.TransparencyKey = colorNeedToAvoid;
+        }
+
+        // Chuc nang resize form
+        protected override void WndProc(ref Message m)
+        {
+            const int wmNcHitTest = 0x84;
+            const int htLeft = 10;
+            const int htRight = 11;
+            const int htTop = 12;
+            const int htTopLeft = 13;
+            const int htTopRight = 14;
+            const int htBottom = 15;
+            const int htBottomLeft = 16;
+            const int htBottomRight = 17;
+
+            if (m.Msg == wmNcHitTest)
             {
-                e.Graphics.FillRectangle(Brushes.Purple, e.CellBounds);
+                int x = (int)(m.LParam.ToInt64() & 0xFFFF);
+                int y = (int)((m.LParam.ToInt64() & 0xFFFF0000) >> 16);
+                Point pt = PointToClient(new Point(x, y));
+                Size clientSize = ClientSize;
+                ///allow resize on the lower right corner
+                if (pt.X >= clientSize.Width - 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htBottomLeft : htBottomRight);
+                    return;
+                }
+                ///allow resize on the lower left corner
+                if (pt.X <= 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htBottomRight : htBottomLeft);
+                    return;
+                }
+                ///allow resize on the upper right corner
+                if (pt.X <= 16 && pt.Y <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htTopRight : htTopLeft);
+                    return;
+                }
+                ///allow resize on the upper left corner
+                if (pt.X >= clientSize.Width - 16 && pt.Y <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htTopLeft : htTopRight);
+                    return;
+                }
+                ///allow resize on the top border
+                if (pt.Y <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htTop);
+                    return;
+                }
+                ///allow resize on the bottom border
+                if (pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htBottom);
+                    return;
+                }
+                ///allow resize on the left border
+                if (pt.X <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htLeft);
+                    return;
+                }
+                ///allow resize on the right border
+                if (pt.X >= clientSize.Width - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htRight);
+                    return;
+                }
             }
-            if (e.Column == 1 && e.Row == 0)
+            base.WndProc(ref m);
+        }
+
+        // Chuc nang keo title de di chuyen form
+        private void PnlTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
             {
-                e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds);
-            }
-            if (e.Column == 2 && e.Row == 0)
-            {
-                e.Graphics.FillRectangle(Brushes.LimeGreen, e.CellBounds);
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
             }
         }
+
+        // Chinh mau sac cho Vien` form
+        private void SetFormBorderColor(Color color)
+        {
+            pnlBorder.BackColor = color;
+        }
+
     }
 }
